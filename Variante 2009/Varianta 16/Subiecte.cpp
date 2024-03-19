683e2ceb4e0ed9b8e18122e827d14475b956b4ae
S͓̽a͓̽m͓̽i͓̽ ͓̽-͓̽ ͓̽S͓̽a͓̽m͓̽i͓̽a͓̽n͓̽ ͓̽-͓̽S͓̽a͓̽m͓̽u͓̽i͓̽a͓̽n͓̽ ͓̽-͓̽S͓̽a͓̽m͓̽a͓̽n͓̽ ͓̽-͓̽S͓̽p͓̽l͓̽ ͓̽-͓̽Z͓̽a͓̽m͓̽-͓̽ ͓̽Z͓̽a͓̽m͓̽a͓̽ ͓̽-͓̽Z͓̽e͓̽m͓̽o͓̽ <150227869+samihavri@users.noreply.github.com>
2024-03-19 12:27:08 +0200

    Update Subiecte.cpp

--- Variante 2009/Varianta 16/Subiecte.cpp
+++ Variante 2009/Varianta 16/Subiecte.cpp
@@ -7,3 +7,22 @@ Zona Vestica : sub diagonala princiapala si deasupra diagonalei secundare;
 i>j si i+j<n+1
 Zona Estica : deasupra diagonalei principale si sub diagonala secundara;
 i<j si i+j>n+1 
+
+
+  problema 3
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{ int n,a[1001],k,z=0,i;
+    cin>>n;
+    for(i=1; i<=n; i++)
+        cin>>a[i];
+    cin>>k;
+    for(i=1; i<=n; i++)
+        if(a[i]%k==0 && a[i]%10==k)
+            z++;
+    cout<<z;
+    return 0;
+}
+problema 4
+
