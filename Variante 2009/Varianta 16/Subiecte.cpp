1b34b36883489db3a155889cfa2d418bce1c3fc5
S͓̽a͓̽m͓̽i͓̽ ͓̽-͓̽ ͓̽S͓̽a͓̽m͓̽i͓̽a͓̽n͓̽ ͓̽-͓̽S͓̽a͓̽m͓̽u͓̽i͓̽a͓̽n͓̽ ͓̽-͓̽S͓̽a͓̽m͓̽a͓̽n͓̽ ͓̽-͓̽S͓̽p͓̽l͓̽ ͓̽-͓̽Z͓̽a͓̽m͓̽-͓̽ ͓̽Z͓̽a͓̽m͓̽a͓̽ ͓̽-͓̽Z͓̽e͓̽m͓̽o͓̽ <150227869+samihavri@users.noreply.github.com>
2024-03-19 12:07:33 +0200

    Create Subiecte.cpp

---
+++ Variante 2009/Varianta 16/Subiecte.cpp
@@ -0,0 +1,9 @@
+problema 5 
+Zona Nordica : deasupra diagonalei principale si deasupra diagonalei secundare;
+i<j si i+j<n+1
+Zona Sudica : sub diagonala principala si sub diagonala secundara;
+i>j si i+j>n+1
+Zona Vestica : sub diagonala princiapala si deasupra diagonalei secundare;
+i>j si i+j<n+1
+Zona Estica : deasupra diagonalei principale si sub diagonala secundara;
+i<j si i+j>n+1 
