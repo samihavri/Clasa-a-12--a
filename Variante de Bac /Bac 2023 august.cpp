9f357f6a6b2a80cb87e6f9af629e83cb9a19b608
S͓̽a͓̽m͓̽i͓̽ ͓̽-͓̽ ͓̽S͓̽a͓̽m͓̽i͓̽a͓̽n͓̽ ͓̽ <150227869+samihavri@users.noreply.github.com>
2024-03-26 10:06:55 +0200

    Update Bac 2023 august.cpp

--- Variante de Bac /Bac 2023 august.cpp
+++ Variante de Bac /Bac 2023 august.cpp
@@ -1,3 +1,105 @@
+
+II . 2
+ #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    struct produs
+    {
+        char denumire[20];
+        int pret;
+
+    }p;
+    char a;
+    cin>>p.denumire;
+    cin>>p.pret;
+    if(p.pret<100)
+    {
+        a=p.denumire[0];
+    } else cout<<"*";
+
+
+    return 0;
+}
+
+
+Prob.1
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void DNPI(int n)
+{
+    int d,i,nr=0;
+    cout<<"1 ";
+    for(i=4; i<=n; i++)
+    {
+        if(n%i==0 && i%2!=0)
+        {
+            for(d=2; d<i; d++)
+            {
+                if(i%d==0)
+                    nr++;
+            }
+            if(nr!=0)
+                cout<<i<<" ";
+        }
+        nr=0;
+    }
+}
+
+
+int main()
+{
+    int a;
+    a=90;
+    DNPI(a);
+
+    return 0;
+}
+
+
+Prob.2
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+ifstream f("date.in");
+
+int main()
+{
+    int v[20][20],i,j,sum=0,maxx=0,m,n;
+    f>>m>>n;
+    for(i=1; i<=m; i++)
+        for(j=1; j<=n; j++)
+            f>>v[i][j];
+    for(i=1; i<m; i++)
+    {
+        for(j=1; j<n; j++)
+        {
+            sum=v[i][j]+v[i][j+1]+v[i+1][j]+v[i+1][j+1];
+            if(sum>maxx)
+                maxx=sum;
+            sum=0;
+        }
+
+    }
+    cout<<maxx;
+
+
+    return 0;
+}
+
+
+
+
+Prob.3
+
 #include <iostream>
 #include <bits/stdc++.h>
 #include <fstream>
