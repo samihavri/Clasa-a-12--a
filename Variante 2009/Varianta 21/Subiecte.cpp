37542428e34998cb98079db06d034d4911f8bdbc
S͓̽a͓̽m͓̽i͓̽ ͓̽-͓̽ ͓̽S͓̽a͓̽m͓̽i͓̽a͓̽n͓̽ ͓̽ <150227869+samihavri@users.noreply.github.com>
2024-04-09 09:36:06 +0300

    Create Subiecte.cpp

---
+++ Variante 2009/Varianta 21/Subiecte.cpp
@@ -0,0 +1,40 @@
+Subiectul 1
+1.b
+2.a)2,833
+b)cat timp i>n && a>0 executa
+c) #include<iostream>
+using namespace std;
+int main()
+{int a,b,n,i;
+cin>>a>>b>>n;
+if(b==0)
+cout<<"gresit";
+else {cout<<a/b;
+if(n>0 && a%b!=0)
+{cout<<",";
+a=a%b;
+i=0;
+do {cout<<(a*10)/b;
+a=(a*10)%b;
+i++;
+while(i==n || a==0)}}
+  d)62,8,4
+Subiectul 2
+    1.b
+    2.c
+    3.ev.data_nasterii_an=1990;
+    5.
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{char a[250];
+int i;
+cin.getline(a,250);
+for(i=1;i<=strlen(a);i++)
+if(a[i-1]=a[i] && a[i]!=’*’)
+cout<<a[i-1]<<a[i]<<endl;
+return 0;}
+Subiectul 3
+1.c
+2.2,1
+3.
