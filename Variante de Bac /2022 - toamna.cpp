199a1f9fb0a5d7ca5b5e6c71cb9c9963c9ffa250
S͓̽a͓̽m͓̽i͓̽ ͓̽-͓̽ ͓̽S͓̽a͓̽m͓̽i͓̽a͓̽n͓̽ ͓̽ <150227869+samihavri@users.noreply.github.com>
2024-04-18 07:35:09 +0300

    Update 2022 - toamna.cpp

--- Variante de Bac /2022 - toamna.cpp
+++ Variante de Bac /2022 - toamna.cpp
@@ -82,7 +82,59 @@ int main()
 }
 
 2. 
+  // problema 2
+#include <bits/stdc++.h>
+
+using namespace std;
+ifstream f("date.in");
+
+int main()
+{
+    char s[50];
+    cin.get(s,50);
+    for(int i=0;i<strlen(s);i++)
+            while(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u')
+                s[i]--;
+    cout<<s;
+    return 0;
+}
+
+3.
   
+//problema 3
+#include <bits/stdc++.h>
+
+using namespace std;
+ifstream f("date.in");
+
+int main()
+{
+    int x,y,max1=0,max2=0,nr1=1,nr2=1;
+    f>>x;
+    while(f>>y)
+    {
+
+        if(x%2==y%2 && y%2==1)
+            nr1++;
+
+        else if(x%2==y%2 && y%2==0)
+            nr2++;
+
+        if(max1<nr1)
+        {
+            max1=nr1;
+            nr1=1;
+        }
+        else if(max2<nr2)
+        {
+            max2=nr2;
+            nr2=1;
+        }
+        x=y;
+    }
+    cout<<max2<<" "<<max1;
+    return 0;
+}
 
 
 
